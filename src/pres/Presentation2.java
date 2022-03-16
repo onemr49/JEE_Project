5364718297c6f8317bdd66401f8afd0fc834d687
onemr49 <101463406+onemr49@users.noreply.github.com>
2022-03-16 23:43:41 +0100

    Full Source Code
    All file in project got committed once

---
+++ src/pres/Presentation2.java
@@ -0,0 +1,37 @@
+package pres;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Scanner;
+
+import dao.Idao;
+import metier.IMetier;
+
+public class Presentation2 {
+
+	
+	public static void main(String[] args) throws FileNotFoundException, ClassNotFoundException, InstantiationException, IllegalAccessException, IllegalArgumentException, InvocationTargetException, NoSuchMethodException, SecurityException
+	{
+		//Injection des dépendances
+		
+		//Instanciation Dinamique
+		
+		String pathname = "config.txt";
+		Scanner sc=new Scanner(new File(pathname));
+		
+		String daoClassName= sc.nextLine();
+		Class<?> cDao=Class.forName(daoClassName); //Charger la classe en mémoire 
+		Idao dao = (Idao) cDao.getDeclaredConstructor().newInstance();
+		
+		String metierClassName= sc.nextLine();
+		Class<?> cMetier=Class.forName(metierClassName); //Charger la classe en mémoire 
+		IMetier metier = (IMetier) cMetier.getDeclaredConstructor().newInstance();
+		
+		//Assosociation
+		Method method=cMetier.getMethod("setDao",Idao.class);
+		method.invoke(metier,dao);
+		System.out.println("Résultat: " + metier.Calcul());
+	}
+}
